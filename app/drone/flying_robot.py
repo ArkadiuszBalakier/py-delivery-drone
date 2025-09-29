a08d29d557a44f3a0f9c897fd1b635ee378249db
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-29 12:49:09 +0200

    implementing flying_robot

---
+++ app/drone/flying_robot.py
@@ -0,0 +1,16 @@
+from app.drone.base_robot import BaseRobot
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int, coords: list[int] = None):
+        if coords is None:
+            coords = [0,0,0]
+        if len(coords) < 3:
+            coords.append(0)
+        super().__init__(name, weight, coords)
+
+    def go_up(self, step: int = 1) -> None:
+        self.coords[2] += step
+
+    def go_down(self, step: int = 1) -> None:
+        self.coords[2] = max(0, self.coords[2] - step)
