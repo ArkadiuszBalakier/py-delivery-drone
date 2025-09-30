2bf883934b9527ef6abc5d271b59bcd605ea9412
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-30 12:02:05 +0200

    fixing bugs from review summary

--- app/drone/delivery_drone.py
+++ app/drone/delivery_drone.py
@@ -2,15 +2,23 @@ from app.drone.flying_robot import FlyingRobot
 from app.drone.cargo import Cargo
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str, weight: int, max_load_weight: int, coords: list[int] | None = None,) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: Cargo | None = None,
+            coords: list[int] | None = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = None
+        self.hook_load(current_load)
 
     def hook_load(self, other: Cargo) -> None:
         if self.current_load is None:
             if self.max_load_weight >= other.weight:
-                self.current_load = other.weight
+                self.current_load = other
         return
 
     def unhook_load(self) -> None:
--- app/drone/flying_robot.py
+++ app/drone/flying_robot.py
@@ -2,12 +2,24 @@ from app.drone.base_robot import BaseRobot
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords = None):
+    def __init__(self, name: str, weight: int, coords: list[int] | None = None) -> None:
         if coords is None:
-            coords = [0,0,0]
-        if len(coords) == 2:
-            coords.append(0)
-        super().__init__(name, weight, coords)
+            coords = [0, 0, 0]
+            x = coords[1]
+            y = coords[0]
+            z = coords[2]
+            self.coords = [x, y, z]
+        if len(coords) <= 2:
+            x = coords[1]
+            y = coords[0]
+            z=0
+            self.coords = [x, y, z]
+        x = coords[1]
+        y = coords[0]
+        z = coords[2]
+        self.coords = [x, y, z]
+
+        super().__init__(name, weight, [x, y])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
